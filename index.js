764c1bd569d55ec3cc6937f5ca24c009e768b292
Brujitari <natimontesg2@mail.com>
2022-09-07 01:46:07 +0200

    feat: messages console logged

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const app = require('express')();
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
 
-io.on('connection', () => { /* … */ });
+server.listen(process.env.PORT, () => {
+  console.info(`😋🌈 te escucho en el ${process.env.PORT} bb 🦋`);
+});
 
-server.listen(3000);
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+// io.on('connection', () => { /* … */ });
+// server.listen(process.env.SOCKET_PORT);

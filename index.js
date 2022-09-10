7823ebb6cf8dba3b7f1c863f1fca9d2c1951d200
Brujitari <natimontesg2@mail.com>
2022-09-11 01:16:59 +0200

    global chat

--- index.js
+++ index.js
@@ -11,16 +11,36 @@ const io = socketio(server, {
     methods: ["GET", "POST"]
   }
 })
-
+//entiendo que esta es la parte que va en las rutas, mandar x todas las rutas q necesiten db, io? .... pero el io.on connection al hacer login?
 io.on('connection', socket => {
-
-  socket.on('connected' , () => {
-    console.log('user connected')
+  let name
+  
+  socket.on('connected' , inputName => {
+    //socket.on todo lo que pongas aquí va a pasar cuando alguien se conecte en ese evento en concreto ('connected')
+    //en front socket.emit('connected', 'huli') sería inicializar una conexión y pasar 'huli' al server
+    name = inputName
+    socket.broadcast.emit('messages', {name, message: `${name} has entered the chat`})
   })
 
-  socket.on('message', (name, message) => {
-    io.emit('messages', {name, message})
+  socket.on('join', room => {
+    console.log(`Socket ${socket.id} with name ${name} joining ${room}`)
+    socket.join(room)
   })
+
+/*   socket.on('chat', (message, room) => {
+    console.log(`msg: ${message}, room: ${room}`);
+    io.to(room).emit('chat', message);
+ }); */
+
+  socket.on('message', (name, message) => { //name y message de socket.emit('message', name, message) in Chat.js 15
+    io.emit('messages', { name, message })  //volvemos a mandar el mensaje, esta vez a todos los clientes
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('messages', {server: 'Server', message: `${name} disconnected`})
+  })
+
+
 })
 
 server.listen(process.env.PORT || 3001, () => {
